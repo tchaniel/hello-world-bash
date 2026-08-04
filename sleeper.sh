6c930c7eadd63755cd33e50f2c2175e42428ada6
tchaniel <35037748+tchaniel@users.noreply.github.com>
2026-08-04 07:30:15 +0000

    Created new sleeper1 bash file

--- sleeper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-while True;
-    do echo "Hangin out...........";
-    sleep 2;
-    done
---
+++ sleeper1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+while True;
+do
+    echo "Hangin out..........."
+    sleep 2
+done
